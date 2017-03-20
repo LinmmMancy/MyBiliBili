4309f2155b6868d27f66913f4981a545514c8042
Mancy <519660797@qq.com>
2017-03-21 01:12:48 +0800

    上下拉

--- app/src/main/java/com/mancy/mybilibili/MainActivity.java
+++ app/src/main/java/com/mancy/mybilibili/MainActivity.java
@@ -4,23 +4,14 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.RelativeLayout;
-
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @InjectView(R.id.toolbar)
-    Toolbar toolbar;
-    @InjectView(R.id.content_main)
-    RelativeLayout contentMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.inject(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("  小明哥");
         toolbar.setLogo(R.drawable.ic_noface);// tou、
