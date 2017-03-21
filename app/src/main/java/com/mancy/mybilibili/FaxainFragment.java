406eef66f63df43b5d903b6b5bc1438b0c645573
Mancy <519660797@qq.com>
2017-03-21 11:11:57 +0800

    框架搭构完成

---
+++ app/src/main/java/com/mancy/mybilibili/FaxainFragment.java
@@ -0,0 +1,35 @@
+package com.mancy.mybilibili;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by linmingming(林明明) on 2017/3/21.
+ * QQ ：519660797
+ * Tel: 17600305015
+ * 作用：
+ */
+public class FaxainFragment extends BaseFragment {
+    private TextView textView;
+
+    @Override
+    public View initView() {
+
+        textView = new TextView(context);
+        textView.setTextSize(30);
+        textView.setGravity(Gravity.CENTER);
+
+        textView.setTextColor(Color.RED);
+
+
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("发现");
+    }
+}
--- app/src/main/java/com/mancy/mybilibili/MainActivity.java
+++ app/src/main/java/com/mancy/mybilibili/MainActivity.java
@@ -2,23 +2,91 @@ package com.mancy.mybilibili;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @InjectView(R.id.fl_main)
+    FrameLayout flMain;
+    @InjectView(R.id.navigation_view)
+    NavigationView navigationView;
+    @InjectView(R.id.id_drawer_layout)
+    DrawerLayout idDrawerLayout;
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+    @InjectView(R.id.tablayout)
+    TabLayout tablayout;
+    @InjectView(R.id.view_pager)
+    ViewPager viewPager;
+
+
+    private ArrayList<Fragment> fragment;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.inject(this);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("  小明哥");
-        toolbar.setLogo(R.drawable.ic_noface);// tou、
+        toolbar.setLogo(R.drawable.tou);// tou、
         toolbar.setTitleTextColor(Color.YELLOW);
         toolbar.setBackgroundColor(Color.parseColor("#FB7299"));
 
+        toolbar.setNavigationIcon(R.drawable.ic_drawer_home);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "123", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        initData();
     }
 
+    private void initData() {
+
+
+        List<String> titles = new ArrayList<>();
+        titles.add(getString(R.string.tab_title_main_1));
+        titles.add(getString(R.string.tab_title_main_2));
+        titles.add(getString(R.string.tab_title_main_3));
+        tablayout.addTab(tablayout.newTab().setText(titles.get(0)));
+        tablayout.addTab(tablayout.newTab().setText(titles.get(1)));
+        tablayout.addTab(tablayout.newTab().setText(titles.get(2)));
+
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(new LiveFragment());
+        fragments.add(new TuijianFragment());
+        fragments.add(new ZhuiFanFragment());
+        fragments.add(new FenFragment());
+        fragments.add(new FaxainFragment());
+
+        viewPager.setOffscreenPageLimit(2);
+
+        FragmentAdapter mFragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), fragments, titles);
+        viewPager.setAdapter(mFragmentAdapter);
+        tablayout.setupWithViewPager(viewPager);
+        tablayout.setTabsFromPagerAdapter(mFragmentAdapter);
+    }
 
 }
